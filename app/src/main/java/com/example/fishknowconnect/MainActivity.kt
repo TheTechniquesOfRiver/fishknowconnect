6b0f8d6912a7732366e053d45b3f2554ab2dcd9a
jyoshna-joshi <g.jyoshnajoshi@gmail.com>
2023-08-15 19:30:14 +1000

    initial set up

---
+++ app/src/main/java/com/example/fishknowconnect/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.fishknowconnect
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.fishknowconnect.register.RegisterActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //splash screen
+        Handler(Looper.getMainLooper()).postDelayed(Runnable {
+            val i = Intent(this@MainActivity, RegisterActivity::class.java)
+            startActivity(i)
+            finish()
+        }, 3000)
+    }
+}
