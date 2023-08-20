a2d3212833cb10ce9875f09e8c688db7f314356c
jyoshna-joshi <g.jyoshnajoshi@gmail.com>
2023-08-21 09:02:08 +1000

    navigation drawer and base dashboard

--- app/src/main/java/com/example/fishknowconnect/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/example/fishknowconnect/ui/register/RegisterActivity.kt
@@ -1,15 +1,21 @@
-package com.example.fishknowconnect.register
+package com.example.fishknowconnect.ui.register
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.example.fishknowconnect.NavigationDrawerActivity
 import com.example.fishknowconnect.R
-
+import com.example.fishknowconnect.ui.home.HomeViewModel
 
 class RegisterActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
 
     }
 }
