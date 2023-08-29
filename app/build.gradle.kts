91d404203eff060244c832fb9331e3df5d7e0805
jyoshna-joshi <g.jyoshnajoshi@gmail.com>
2023-08-29 22:51:28 +1000

    implemented initial registration features

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,24 +5,26 @@ plugins {
 
 android {
     namespace = "com.example.fishknowconnect"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.fishknowconnect"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
             proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
             )
         }
     }
@@ -36,10 +38,39 @@ android {
     buildFeatures {
         viewBinding = true
         dataBinding = true
+        compose = true
+
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.8"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
     }
 }
 
 dependencies {
+
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.compose.ui:ui-graphics")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    //compose
+    val composeBom = platform("androidx.compose:compose-bom:2023.08.00")
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui")
+    implementation ("androidx.activity:activity-compose:1.8.0-alpha07")
+
+    // Android Studio Preview support
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    // Integration with activities
+    implementation("androidx.activity:activity-compose:1.7.3")
+    implementation("androidx.activity:activity-ktx:1.7.2")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
@@ -48,20 +79,24 @@ dependencies {
 
     implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
     implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
 //    implementation ("com.squareup.retrofit2:converter-scalars:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     // LiveData
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation ("androidx.compose.runtime:runtime-livedata:1.0.0-beta01")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
-    implementation ("androidx.lifecycle:lifecycle-viewmodel:$2.2.0-alpha01")
+    implementation("androidx.lifecycle:lifecycle-viewmodel:$2.2.0-alpha01")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
-    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:$2.2.0-alpha01")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$2.2.0-alpha01")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     //logging
     implementation("com.squareup.okhttp3:logging-interceptor:3.9.0")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
 
 }
--- app/src/main/java/com/example/fishknowconnect/network/FishKnowConnectApiService.kt
+++ app/src/main/java/com/example/fishknowconnect/network/FishKnowConnectApiService.kt
@@ -1,6 +1,7 @@
 package com.example.fishknowconnect.network
 
 import com.example.fishknowconnect.ui.login.LoginResponse
+import com.example.fishknowconnect.ui.register.RegisterResponse
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Response
@@ -10,7 +11,8 @@ import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
-private const val BASE_URL = "http://10.0.2.2:1000/"
+private const val BASE_URL = "http://13.236.94.194:3000/"
+//private const val BASE_URL = "http://10.0.2.2:3000/"
 private val logging = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
 private val client = OkHttpClient.Builder().addInterceptor(logging).build()
 private val retrofit =
@@ -23,6 +25,15 @@ interface FishKnowConnectApiService {
     suspend fun login(
         @Field("username") username: String, @Field("password") password: String
     ): Response<LoginResponse>
+
+    @FormUrlEncoded
+    @POST("register")
+    suspend fun register(
+        @Field("username") username: String,
+        @Field("password") password: String,
+        @Field("phone") phone: String,
+        @Field("ID") id: String
+    ): Response<RegisterResponse>
 }
 
 object FishKnowConnectApi {
