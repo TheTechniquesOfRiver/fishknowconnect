0228fd9c7c5352ef3e855564a01180ee1b58dd77
jyoshna-joshi <g.jyoshnajoshi@gmail.com>
2023-09-09 18:21:25 +1000

    created new post layout for all sections

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,8 @@ android {
 }
 
 dependencies {
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     val nav_version = "2.7.1"
 
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
--- app/src/main/java/com/example/fishknowconnect/ui/water/WaterFragment.kt
+++ app/src/main/java/com/example/fishknowconnect/ui/water/WaterFragment.kt
@@ -3,15 +3,21 @@ package com.example.fishknowconnect.ui.fish
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnClickListener
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.fishknowconnect.R
 import com.example.fishknowconnect.databinding.FragmentWaterBinding
 
-class WaterFragment : Fragment() {
+class WaterFragment : Fragment(), OnClickListener{
 
     private var _binding: FragmentWaterBinding? = null
+    private var navController: NavController? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -30,8 +36,22 @@ class WaterFragment : Fragment() {
         return root
     }
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view);
+        view.findViewById<Button>(R.id.buttonNewPost).setOnClickListener(this)
+
+    }
+
+
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onClick(v: View?) {
+        navController?.navigate(R.id.action_waterFragment_to_newPostActivity)
     }
 }
